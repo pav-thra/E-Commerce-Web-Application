75892cd060de5bb9bcdaafb4e517648420d4c3fe
Pavithra <v.pavithrav1011@gmail.com>
2024-08-16 17:24:23 +0530

    Get Mapping: to get a product using its id
    Put mapping: to accept product as JSON, convert it to an actual product and save it to database

--- src/main/java/org/example/CRUDapplication/Product/ProductController.java
+++ src/main/java/org/example/CRUDapplication/Product/ProductController.java
@@ -2,14 +2,11 @@ package org.example.CRUDapplication.Product;
 
 import org.example.CRUDapplication.Product.Model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 //tells spring boot to act like a controller : use annotations
 @RestController
@@ -37,6 +34,28 @@ public class ProductController
         //.status() - used to set the HTTP status
         //response body - here it returns an empty list as the body of the response
         //return ResponseEntity.ok().build(); - Creates a ResponseEntity with a 200 OK status and no body.
+    }
+
+    //to get specific product using its id
+    //means when this mapping in pinged add the id you are looking for
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProduct(@PathVariable Integer id)
+    //means whatever endpoint is there in get mapping pass it into the function
+    //make sure getMapping id and path variable id var name is same
+    {
+        return ResponseEntity.ok(productRepository.findById(id));
+        //optional product: means like what if it is not there then?
+    }
+
+    //accept product as JSON, convert it to an actual product and save it to database
+    @PostMapping
+    public ResponseEntity createProduct(@RequestBody Product product)
+    {
+        //to tell Spring Boot, product comes in the body of the request tell it @request body
+        //call product repo and built in save method to save product
+        productRepository.save(product);
+
+        return ResponseEntity.ok().build();
 
     }
 }
